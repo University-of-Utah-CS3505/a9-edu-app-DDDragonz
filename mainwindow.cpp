0826529cf19d34f44d29db4427aaa84c48fe0b8b
DDDragonz <Gmclaughlin52@outlook.com>
2023-11-29 10:29:33 -0700

    Implemented moveable vial

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,95 +1,259 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-    , world(b2Vec2(0.0f, -10.0f))
+    , world(new b2World(b2Vec2(0.0f, -10.0f)))
     , timer(new QTimer(this))
 {
     ui->setupUi(this);
-
-    // Define the ground body.
-    b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, -10.0f);
-
-    // Call the body factory which allocates memory for the ground body
-    // from a pool and creates the ground box shape (also from a pool).
-    // The body is also added to the world.
-    b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-    // Define the ground box shape.
-    b2PolygonShape groundBox;
-
-    // The extents are the half-widths of the box.
-    groundBox.SetAsBox(50.0f, 10.0f);
-
-    // Add the ground fixture to the ground body.
-    groundBody->CreateFixture(&groundBox, 0.0f);
-
-    // Define the dynamic body. We set its position and call the body factory.
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 4.0f);
-    body = world.CreateBody(&bodyDef);
-
-    // Define another box shape for our dynamic body.
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
-
-    // Define the dynamic body fixture.
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-
-    // Set the box density to be non-zero, so it will be dynamic.
-    fixtureDef.density = 1.0f;
-
-    // Override the default friction.
-    fixtureDef.friction = 0.3f;
-
-    // Determines the bounciness
-    fixtureDef.restitution = 0.9f;
-
-    // Add the shape to the body.
-    body->CreateFixture(&fixtureDef);
-
-    //connections
-    connect(this,
-            &MainWindow::newHeight,
-            ui->slider,
-            &QSlider::setValue);
+    windowWidth = this->width();
+    qDebug() << windowWidth;
+    windowHeight = this->height() - 50;
 
     connect(timer,
             &QTimer::timeout,
             this,
-            &MainWindow::updateWorld);
+            &MainWindow::Update);
 
-    timer->start(1000/60);
+
+    connect(ui->reset,
+            &QPushButton::clicked,
+            this,
+            &MainWindow::resetScene);
+
+    createScene();
 }
 
-void MainWindow::updateWorld()
+void MainWindow::createBorder()
 {
-    // Prepare for simulation. Typically we use a time step of 1/60 of a
-    // second (60Hz) and 10 iterations. This provides a high quality simulation
-    // in most game scenarios.
+    //Define body
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_kinematicBody;
+    bodyDef.position.Set(0, 0);
+    b2Body* body = world->CreateBody(&bodyDef);
+    b2EdgeShape edge;
+
+    // Bottom border
+    edge.Set(b2Vec2(0, 0), b2Vec2(windowWidth / SCALE, 0));
+    body->CreateFixture(&edge, 0);
+
+    // Left border
+    edge.Set(b2Vec2(0, 0), b2Vec2(0, windowHeight / SCALE));
+    body->CreateFixture(&edge, 0);
+
+    // Top border
+    edge.Set(b2Vec2(0, windowHeight / SCALE), b2Vec2(windowWidth / SCALE, windowHeight / SCALE));
+    body->CreateFixture(&edge, 0);
+
+    // Right border
+    edge.Set(b2Vec2(windowWidth / SCALE, 0), b2Vec2(windowWidth / SCALE, windowHeight / SCALE));
+    body->CreateFixture(&edge, 0);
+}
+
+void MainWindow::paintEvent(QPaintEvent*)
+{
+    QPainter painter(this);
+    for (b2Body* body = world->GetBodyList(); body != 0; body = body->GetNext())
+    {
+        for (b2Fixture* fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext())
+        {
+            b2Shape* shape = fixture->GetShape();
+            if (shape->GetType() == b2Shape::e_edge)
+            {
+                drawEdge(painter, body, (b2EdgeShape*)shape);
+            }
+            else if (shape->GetType() == b2Shape::e_circle)
+            {
+                drawCircle(painter, body, (b2CircleShape*)shape);
+            }
+            else if (shape->GetType() == b2Shape::e_polygon)
+            {
+                drawPolygon(painter, body, (b2PolygonShape*)shape);
+            }
+        }
+    }
+}
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        b2Vec2 pos(event->pos().x() / SCALE, (windowHeight - event->pos().y()) / SCALE);
+        b2Vec2 velocity = pos - vial->GetPosition();
+        float velocityScale = 3.0f; //Changeable
+        velocity *= velocityScale;
+        vial->SetLinearVelocity(velocity);
+        update();
+    }
+}
+
+void MainWindow::createVial()
+{
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    //bodyDef.fixedRotation = true;
+    bodyDef.bullet = true;
+    bodyDef.position.Set(windowWidth  / 4 / SCALE, windowHeight / 3 / SCALE);
+    vial = world->CreateBody(&bodyDef);
+
+    b2Vec2 vertices[4];
+    vertices[0].Set(-1.0f, -3.0f); //bottom-left
+    vertices[1].Set(-1.0f, 3.0f); //top-left
+    vertices[2].Set(1.0f, 3.0f); //top-right
+    vertices[3].Set(1.0f, -3.0f); //bottom-right
+
+    createWall(vial, vertices[0], vertices[1]); // Left wall
+    createWall(vial, vertices[2], vertices[3]); // Right wall
+    createWall(vial, vertices[0], vertices[3]); // Bottom wall
+}
+
+void MainWindow::SpawnBox()
+{
+    // Define a body
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_kinematicBody; // Set the body to be dynamic
+    bodyDef.position.Set(windowWidth  / 2 / SCALE, windowHeight / 2 / SCALE);
+    b2Body *body = world->CreateBody(&bodyDef);
+
+    // Assign a rectangular shape to the body
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(0.5f, 1.0f);
+
+    // Define fixture
+    b2FixtureDef fixture;
+    fixture.shape = &boxShape; // Assign shape to fixture
+    fixture.density = 1.0f;
+    fixture.friction = 0.3f;
+    fixture.restitution = 0.3f;
+
+    // Attach the fixture to the body
+    body->CreateFixture(&fixture);
+}
+
+
+void MainWindow::SpawnCircle()
+{
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.linearDamping = 2.0f;
+    bodyDef.angularDamping = 1.0f;
+
+    bodyDef.position.Set(windowWidth  / 2 / SCALE, windowHeight / 2 / SCALE);
+    b2Body* particle = world->CreateBody(&bodyDef);
+    b2CircleShape dynamicCircle;
+    dynamicCircle.m_radius = 0.1f;
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicCircle;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 20.0f;
+    fixtureDef.restitution = 0.01f;
+    particle->CreateFixture(&fixtureDef);
+}
+
+
+void MainWindow::createWall(b2Body* body, b2Vec2 vertex1, b2Vec2 vertex2)
+{
+    b2PolygonShape wall;
+    float thickness = 0.1f;
+    b2Vec2 direction = vertex2 - vertex1;
+    b2Vec2 offset(direction.y, -direction.x);
+    offset.Normalize();
+    offset *= thickness / 2;
+    b2Vec2 vertices[4] = {vertex1 - offset, vertex2 - offset, vertex2 + offset, vertex1 + offset};
+    wall.Set(vertices, 4);
+
+    b2FixtureDef fixture;
+    fixture.shape = &wall;
+    fixture.density = 1.0f;
+    fixture.friction = 0.3f;
+    fixture.restitution = 0.3f;
+    body->CreateFixture(&fixture);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_T)
+    {
+        qDebug() << "Rotate";
+        update();
+    }
+}
+
+void MainWindow::createScene()
+{
+    SpawnBox();
+    SpawnCircle();
+    createBorder();
+    createVial();
+    update();
+    timer->start(1000 / 60);
+}
+
+void MainWindow::resetScene()
+{
+    timer->stop();
+    for (b2Body* body = world->GetBodyList(); body != 0; body = body->GetNext())
+    {
+        world->DestroyBody(body);
+    }
+    world->Step(1/60.f, 8, 3); // make sure everything is deleted
+    b2Vec2 gravity(0.0f, -10.0f);
+    if(world != nullptr)
+    {
+        delete world;
+        world = nullptr;
+    }
+    world = new b2World(gravity);
+    createScene();
+}
+
+void MainWindow::drawEdge(QPainter& painter, b2Body* body, b2EdgeShape* edge)
+{
+    QPointF v1 = convertCoordsBox2DToQt(body->GetWorldPoint(edge->m_vertex1));
+    QPointF v2 = convertCoordsBox2DToQt(body->GetWorldPoint(edge->m_vertex2));
+    painter.drawLine(v1, v2);
+}
+
+void MainWindow::drawPolygon(QPainter& painter, b2Body* body, b2PolygonShape* polygon)
+{
+    QPolygonF qpolygon;
+    for (int32 i = 0; i < polygon->m_count; ++i)
+    {
+        QPointF v = convertCoordsBox2DToQt(body->GetWorldPoint(polygon->m_vertices[i]));
+        qpolygon.append(v);
+    }
+    painter.drawPolygon(qpolygon);
+}
+
+void MainWindow::drawCircle(QPainter& painter, b2Body* body, b2CircleShape* circle)
+{
+    QPointF position = convertCoordsBox2DToQt(body->GetPosition());
+    painter.drawEllipse(position, circle->m_radius * SCALE, circle->m_radius * SCALE);
+}
+
+QPointF MainWindow::convertCoordsBox2DToQt(b2Vec2 vec)
+{
+    vec.x *= SCALE;
+    vec.y = windowHeight - vec.y * SCALE;
+    return QPointF(vec.x, vec.y);
+}
+
+void MainWindow::Update()
+{
     float32 timeStep = 1.0f / 60.0f;
     int32 velocityIterations = 6;
     int32 positionIterations = 2;
-
-    // Instruct the world to perform a single step of simulation.
-    // It is generally best to keep the time step and iterations fixed.
-    world.Step(timeStep, velocityIterations, positionIterations);
-
-    b2Vec2 position = body->GetPosition();
-    float32 angle = body->GetAngle();
-
-    printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-    emit newHeight(position.y * 100);
+    world->Step(timeStep, velocityIterations, positionIterations);
+    update();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
     delete timer;
+    delete world;
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,11 @@
 #include <Box2D/Box2D.h>
 #include <QTimer>
 
+#include <QPaintEvent>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QKeyEvent>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,16 +22,40 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-signals:
-    void newHeight(float value);
+    void paintEvent(QPaintEvent*) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+    void createVial();
+    void createVialLiquid(int numParticles);
+    void createBorder();
+    void createGround();
+    void createWall(b2Body* body, b2Vec2 vertex1, b2Vec2 vertex2);
+    void drawEdge(QPainter& painter, b2Body* body, b2EdgeShape* edge);
+    void drawCircle(QPainter& painter, b2Body* body, b2CircleShape* circle);
+    void drawPolygon(QPainter& painter, b2Body* body, b2PolygonShape* polygon);
+    void resetScene();
+    void SpawnCircle();
+    void createScene();
+
+
+
+    void SpawnBox();
 
 public slots:
-    void updateWorld();
+    void Update();
 
 private:
     Ui::MainWindow *ui;
-    b2World world;
-    b2Body *body;
+    b2World *world;
     QTimer *timer;
+    b2Body* vial;
+
+    const float SCALE = 32.0f;
+    float windowWidth;
+    float windowHeight;
+
+    /// Box2D to Qt (meters to pixels), you multiply by SCALE.
+    /// Qt to Box2D (pixels to meters), you divide by SCALE.
+    QPointF convertCoordsBox2DToQt(b2Vec2 vec);
 };
 #endif // MAINWINDOW_H
